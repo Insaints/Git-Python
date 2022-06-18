2c43450c65e5fdfab5c37f2a151a067bd268b05a
Aleks Liepin <AleksLiepin@gmail.com>
2022-06-18 22:21:14 +0300

    feat: change function

--- stepik.py
+++ stepik.py
@@ -1,24 +1,10 @@
 def is_valid_password(password):
-    s = psw.split(':')
-    flag1 = True
-    flag2 = True
-    flag3 = True
-    flag4 = True
-    if len(s) != 3:
-        flag1 = False
-    a = s[0]
-    b = int(s[1])
-    c = int(s[2])
-    if a != a[::-1] or c % 2 != 0:
-        flag2 = False
-    if b < 2:
-        flag3 = False
-    for i in range(2, b):
-        if b % i == 0:
-            flag4 = False
-    return flag1 and flag2 and flag3 and flag4
+    password = password.split(':')
+    return (password[0] == password[0][::-1]) and \
+    (len([i for i in range(1, int(password[1])+1) if int(password[1]) % i == 0]) == 2) and \
+    (int(password[2]) % 2 == 0)
 
 psw = input()
 
-print(is_valid_password(psw))
 
+print(is_valid_password(psw))
