65e0490840f398441530453a2192519e70e681c8
Aleks Liepin <AleksLiepin@gmail.com>
2022-06-19 17:27:38 +0300

    exam decision is_pangram

--- stepik.py
+++ stepik.py
@@ -1,24 +1,18 @@
-def is_valid_password(password):
-    s = psw.split(':')
-    flag1 = True
-    flag2 = True
-    flag3 = True
-    flag4 = True
-    if len(s) != 3:
-        flag1 = False
-    a = s[0]
-    b = int(s[1])
-    c = int(s[2])
-    if a != a[::-1] or c % 2 != 0:
-        flag2 = False
-    if b < 2:
-        flag3 = False
-    for i in range(2, b):
-        if b % i == 0:
-            flag4 = False
-    return flag1 and flag2 and flag3 and flag4
+# объявление функции
+def is_pangram(text):
+    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    text = list(text.lower())
+    for i in abc:
+        if i not in text:
+            return False
+    return True
 
-psw = input()
 
-print(is_valid_password(psw))
 
+    print(text)
+
+# считываем данные
+text = input()
+
+# вызываем функцию
+print(is_pangram(text))
